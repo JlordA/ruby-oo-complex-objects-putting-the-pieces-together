1e81c623481cb2435b134f1e67cff145d01387c9
JlordA <james.ardery@gmail.com>
2020-10-06 18:11:29 -0400

    working in OO Ruby

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,60 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+
+     #shoe.condition = "new"
+    # binding.pry
+    
+
+    # def brand=(brand)
+    #     @brand = brand
+    # end
+
+    # def brand
+    #     @brand
+    # end
+
+    # def color=(color)
+    #     @color = color
+    # end
+
+    # def color
+    #     @color
+    # end
+
+    # def size=(size)
+    #     @size = size
+    # end
+
+    # def size
+    #     @size
+    # end
+
+    # def material=(material)
+    #     @material = material
+    # end
+
+    # def material
+    #     @material
+    # end
+
+    # def condition=(condition)
+    #     @condition = condition
+    # end
+     
+    # def condition
+    #     @condition
+    # end
+
+end
